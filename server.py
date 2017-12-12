60bb0ed7b4775cbd41c83c3fe14de41eaf4b13ef
Frances Liu <frances.liu168@gmail.com>
2017-12-12 02:13:01 -0800

    cleaned up code and updated docstrings

--- server.py
+++ server.py
@@ -10,19 +10,35 @@ api = Api(app)
 class tictactoe(Resource):
 
     def get(self):
-        board = request.args.get('board')
-        if self.validify(board):
-            if self.who_turn(board) is 'o':
-                board = self.take_turn(board)
-            return {'board': board}
-        else:
-            return {'empty': 'very empty'}
+        """GET request for TICTACTOE endpoint
+
+        Accepts a board parameter, checks if it's a valid board, plays the next best move to win the game.
+
+        Parameters:
+
+        board - string
+        """
+            board = request.args.get('board')
+            if self.validify(board):
+                if self.who_turn(board) is 'o':
+                    board = self.take_turn(board)
+                return {'board': board}
+            else:
+                return {'empty': 'very empty'}
 
     def validify(self, board):
-        """Verify's this is a valid board
-        Checks for a winner
-        Checks if board has less than 9 characters or empty
-        """
+      """Verify this is a valid board
+      
+      Checks for a winner
+      Checks if board has less than 9 characters or empty
+      Checks if one player is move moves ahead of the other
+      
+      Arguments:
+        board {string} -- String of the board
+      
+      Returns:
+        bool -- True if valid board
+      """
         o_spaces = [iterator for iterator,
                     item in enumerate(board) if item == 'o']
         x_spaces = [iterator for iterator,
@@ -39,6 +55,14 @@ class tictactoe(Resource):
         return True
 
     def is_winning_combo(self, board):
+      """Checks if the board has a winning combo
+      
+      Arguments:
+        board {string} -- String of the board
+      
+      Returns:
+        bool -- True if there's a winning combo
+      """
         winning_combos = [[6, 7, 8], [3, 4, 5], [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 5, 8],
                           [0, 4, 8], [2, 4, 6]]
         for combo in winning_combos:
@@ -53,33 +77,39 @@ class tictactoe(Resource):
         return 'o'
 
     def take_turn(self, board):
-        """Allows the server to make a random move
+        """Allows the server to make a move
+        
+        If the board is empty, choose a random space
+        Currently using choice since there's no logic
+        
+        Returns:
+          [string] -- Updated board with new move
         """
-        # if the board is empty, choose a random space
         if board.isspace():
             new_board = list(board)
             turn = randint(0, 8)
             new_board[turn] = 'o'
             return ''.join(new_board)
         new_board = list(board)
-        # currently using choice since there's no logic
         turn = choice(self.find_empty(board))
-        # double check that the space is empty and mark it with 'o'
         if new_board[turn] == ' ':
             new_board[turn] = 'o'
             new_board = ''.join(new_board)
-            # checks if this is the winning move
             if self.is_winning_combo(new_board) or self.is_winning_combo(new_board):
-              print 'winning move'
+                print 'winning move'
             return new_board
         else:
             self.take_turn(board)
 
     def find_empty(self, board):
-        """Finds empty spaces on board
+        """Finds empty spaces on board and suggests the next best move
+        
         TODO: add logic that chooses the best space on the board
         give each move a score (how close it gets to a winning combo)
-        rank each move and return the highest ranked move
+        rank each move and return the single highest ranked move as an int
+        
+        Returns:
+          [list] -- recommended move 
         """
         new_board = list(board)
         empty_spaces = [iterator for iterator,
